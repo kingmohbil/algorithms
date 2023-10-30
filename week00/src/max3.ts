10a4ffa2a77c79f0034a921c03c6c75771d05f01
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-10-30 12:45:26 +0300

    Added the source folder, added the solution for TypeScript and JavaScript

---
+++ week00/src/max3.ts
@@ -0,0 +1,35 @@
+import { createInterface } from 'readline';
+
+// initializing the input, output interface
+const readline = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+// reading a line form the cli and converting it to a number
+const readLineNumberAsync: (msg: string) => Promise<number> = (msg: string) => {
+  return new Promise((resolve) => {
+    readline.question(msg, (userRes: string) => {
+      resolve(Number(userRes));
+    });
+  });
+};
+
+// a function to calculate the max number between three numbers
+function max(number1: number, number2: number, number3: number): number {
+  let max = number1;
+  if (number2 > max) max = number2;
+  if (number3 > max) max = number3;
+  return max;
+}
+
+async function run() {
+  const message = 'Enter a number: ';
+  const number1 = await readLineNumberAsync(message);
+  const number2 = await readLineNumberAsync(message);
+  const number3 = await readLineNumberAsync(message);
+
+  console.log(max(number1, number2, number3));
+}
+
+run();
