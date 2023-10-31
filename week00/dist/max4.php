6c503307212a414aae785904ba8f0feb11670b70
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-10-31 21:54:14 +0300

    Added the max four algorithm in PHP

---
+++ week00/dist/max4.php
@@ -0,0 +1,18 @@
+<?php
+function max3($number1, $number2, $number3, $number4) {
+    $max = $number1;
+    if($number2 > $max) $max = $number2;
+    if($number3 > $max) $max = $number3;
+    if($number4 > $max) $max = $number4;
+    
+    return $max;
+}
+
+// getting the values for the max4 algorithm
+$number1 = (float) readline("Enter a number: ");
+$number2 = (float) readline("Enter a number: ");
+$number3 = (float) readline("Enter a number: ");
+$number4 = (float) readline("Enter a number: ");
+
+// giving the values for the max4 algorithm
+echo 'The maximum number is: ' . max($number1, $number2, $number3, $number4);
