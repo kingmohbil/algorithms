638d68ce22a8a705384c2bbcdd07a57228980e1a
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-10-31 22:28:39 +0300

    Added the solution using Python for the sum3 algorithm

---
+++ week01/dist/sum3.py
@@ -0,0 +1,11 @@
+def sum(number1, number2, number3):
+    return number1 + number2 + number3
+
+
+# getting the inputs for the sum3 algorithm
+number1 = float(input('Enter a number: '))
+number2 = float(input('Enter a number: '))
+number3 = float(input('Enter a number: '))
+
+# giving the results of the sum3 algorithm
+print(f"The sum of the numbers is: {sum(number1, number2, number3)}")
