e6a1216ceb682a648816c08dbcb6540d14177a68
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-10-31 22:31:22 +0300

    fixed the php files

--- week00/dist/max3.php
+++ week00/dist/max3.php
@@ -1,5 +1,5 @@
 <?php
-function max3($number1, $number2, $number3) {
+function max($number1, $number2, $number3) {
     $max = $number1;
     if($number2 > $max) $max = $number2;
     if($number3 > $max) $max = $number3;
--- week00/dist/max4.php
+++ week00/dist/max4.php
@@ -1,5 +1,5 @@
 <?php
-function max3($number1, $number2, $number3, $number4) {
+function max($number1, $number2, $number3, $number4) {
     $max = $number1;
     if($number2 > $max) $max = $number2;
     if($number3 > $max) $max = $number3;
