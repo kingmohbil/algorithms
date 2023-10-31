88f0bf435723fb2b327fa2c5f2adf94c78599ef3
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-10-31 22:32:48 +0300

    Added the solution using PHP for the sum3 algorithm

---
+++ week01/dist/sum3.php
@@ -0,0 +1,16 @@
+<?php
+function sum($number1, $number2, $number3) {
+    $max = $number1;
+    if($number2 > $max) $max = $number2;
+    if($number3 > $max) $max = $number3;
+    
+    return $max;
+}
+
+// getting the values for the sum3 algorithm
+$number1 = (float) readline("Enter a number: ");
+$number2 = (float) readline("Enter a number: ");
+$number3 = (float) readline("Enter a number: ");
+
+// giving the values for the sum3 algorithm
+echo 'The sum of the numbers is: ' . sum($number1, $number2, $number3);
