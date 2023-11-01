ec5fe7dc8f73ddc9e24ac364f797e3f15bb249cd
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-11-01 12:00:36 +0300

    Solved the division algorithm using Python

---
+++ week01/dist/python/division.py
@@ -0,0 +1,10 @@
+def div(number1, number2):
+    return number1 / number2
+
+
+# getting the inputs for the division algorithm
+number1 = float(input('Enter the first number: '))
+number2 = float(input('Enter the second number: '))
+
+# giving the results of the division algorithm
+print(f"The division of {number1} / {number2} is: {div(number1, number2)}")
